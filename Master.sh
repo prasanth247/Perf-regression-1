13e0b576d2bdeddf0c398b812c81666d17ae4596
prasanth247 <32734886+prasanth247@users.noreply.github.com>
2020-01-22 11:01:11 +0530

    verify JMeter docker images exits

--- Master.sh
+++ Master.sh
@@ -3,9 +3,11 @@
 
 pd=`pwd`
 StartDate=`date +"%m%d%Y%H%M%S"`
-
-wget http://apachemirror.wuchna.com//jmeter/binaries/apache-jmeter-5.2.1.zip
-unzip apache-jmeter-5.2.1.zip
-JMETER_HOME=${pd}/apache-jmeter-5.2.1/bin
-echo `ls $JMETER_HOME`
-
+#cd ../docker-jmeter
+#./build.sh
+ls /opt/apache-jmeter-5.1.1/bin
+echo "done"
+#wget http://apachemirror.wuchna.com//jmeter/binaries/apache-jmeter-5.2.1.zip
+#unzip apache-jmeter-5.2.1.zip
+#JMETER_HOME=${pd}/apache-jmeter-5.2.1/bin
+#echo `ls $JMETER_HOME``
